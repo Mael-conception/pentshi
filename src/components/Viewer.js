7635ca7cdcd371df8a37dcfc5af15f6fac52063d
vanotis720 <vanotis720@gmail.com>
2023-06-15 14:05:02 +0200

    images viewer with sliding and go back

--- src/components/Viewer.js
+++ src/components/Viewer.js
@@ -1,18 +1,52 @@
-import React from "react";
-import { StatusBar, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, StatusBar, StyleSheet, Text, View } from "react-native";
 import colors from "../styles/colors";
+import ImageView from "react-native-image-viewing";
 
 
-export default function Viewer({ route }) {
+export default function Viewer({ navigation, route }) {
 
     const { url } = route.params;
+    const [images, setImages] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
-    return (
-        <View style={styles.container}>
-            <StatusBar barStyle="light-content" backgroundColor={colors.chamoisee} />
+    useEffect(() => {
+        const files = [];
 
-        </View>
-    )
+        url.forEach(element => {
+            files.push({
+                uri: element
+            })
+        });
+
+        setImages(files);
+        setIsLoading(false);
+    }, [])
+
+    if (isLoading) {
+        return (
+            <View style={[styles.container, { justifyContent: 'center', alignContent: 'center' }]}>
+                <ActivityIndicator size={'large'} color={colors.chamoisee} />
+            </View>
+        )
+    }
+
+    if (!isLoading) {
+        return (
+            <View style={styles.container}>
+                <StatusBar barStyle="light-content" backgroundColor={colors.chamoisee} />
+                <ImageView
+                    visible={true}
+                    images={images}
+                    imageIndex={0}
+                    presentationStyle={'fullScreen'}
+                    onRequestClose={() => navigation.goBack()}
+                />
+            </View>
+        )
+    }
+
+
 }
 
 const styles = StyleSheet.create({
